7f0e6c71811d2b2b23ac94b80bab8c0cb8fb7d5f
Rick Schultz <rick-schultz@hotmail.com>
2021-08-19 15:04:56 -0300

    first commit

---
+++ bundle.js
@@ -0,0 +1,7 @@
+$(document).ready(function (e) {
+  $('#head').load('./components/head.html');
+  $('#root').addClass('bg-white font-family-karla align-center');
+  $('#nav').load('./components/nav.html');
+  $('#main').load('./components/main.html');
+  $('#footer').load('./components/footer.html');
+});
